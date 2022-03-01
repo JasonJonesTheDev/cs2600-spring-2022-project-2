49163a13c855323272190ebae2f4d635897ff4c3
Jason Jones <jasojone@gmail.com>
2022-03-01 06:44:03 -0800

    adds params, main, .h

--- main.c
+++ main.c
@@ -14,38 +14,31 @@ int arrival;
 float airFair;
 
 // The amount of any car rentals 
-
 float carRental;
 
 // Miles driven, if a private vehicle was used. 
 // Calculate the vehicle expense as $0.27 per mile driven 
-
 int milesDriven;
 float milesReimbers;
 
 
 // Parking fees (The company allows up to $6 per day. 
 // Anything more than this must be paid by the employee.) 
-
 float parkingFees;
 
 // Taxi fees, if a taxi was used anytime during the trip 
 // (The company allows up to $10 per day, for each day a taxi was used. 
 // Anything more than this must be paid by the employee.) 
-
 float taxiFees;
 
 // Conference or seminar registration fees 
-
 float registraionFees;
  
 // Hotel expenses (The company allows up to $90 per night for lodging. 
 // Anything more than this must be paid by the employee.) 
-
 float hotelExpense;
 
-// The amount of each meal eaten. 
-
+// The amount of each meal eaten and total meal cost after compensation.
 float breakfast;
 float lunch;
 float dinner;
