bd27687b3e0db3c02ea1949b7abcc63ece7de90c
Abigail Pinkus <arpinkus@cpp.edu>
2022-03-03 21:53:29 -0800

    Error with scanf

--- car.c
+++ car.c
@@ -13,25 +13,6 @@
     4. taxi fees
 */
 
-void AskUserForCarInputs() {
-    float carRental;
-    int milesDriven;
-    float parkingFees;
-    int parkingDays;
-    printf("Enter any car rental fees:");
-    scanf("%d\n", &carRental);
-    printf("Enter your total miles driven:");
-    scanf("%d\n", &milesDriven);
-    printf("Enter the number of days you used parking:");
-    scanf("%d\n", &parkingDays);
-    if (parkingDays <= 0) {
-        parkingFees = 0;
-    } else {
-        printf("Enter your total parking fees:");
-        scanf("%d\n", &parkingFees);
-    }
-}
-
 float carRentalFees(float carRental) {
     carRental = carRental;
 
@@ -57,10 +38,42 @@ float calculateParkingFees(float parkingFees, int parkingDays) {
     return parkingFees;
 }
 
+void printCar(float carRental, int milesDriven, float parkingFees, int parkingDays) {
+    printf("\nCar Rental Fees: %d\n", carRentalFees(carRental));
+    printf("Miles Costs: %d\n", mileFees(milesDriven));
+    printf("ParkingFees: %d\n", calculateParkingFees(parkingFees, parkingDays));
+}
+
 /* **This is used to check my code on my local side */
-// void main(float carRental, int milesDriven, float parkingFees, int parkingDays) {
-//     AskUserForCarInputs();
-//     printf("Car Rental Fees: %d\n", carRentalFees(carRental));
-//     printf("Miles Costs: %d\n", mileFees(milesDriven));
-//     printf("ParkingFees: %d\n", calculateParkingFees(parkingFees, parkingDays));
-// }
+void main() {
+    float carRental;
+    int milesDriven;
+    float parkingFees;
+    int parkingDays;
+    char yesOrNo;
+
+    do{
+        printf("\nDid you rent a car?(y or n):");
+        scanf("%s", yesOrNo);
+        if(yesOrNo, "y") {
+            printf("\nEnter any car rental fees:");
+            scanf("%d\n", &carRental);
+        } else {
+            carRental = 0;
+        }
+
+        printf("\nEnter your total miles driven:");
+        scanf("%d\n", &milesDriven);
+
+        printf("\nEnter the number of days you used parking:");
+        scanf("%d\n", &parkingDays);
+        if (parkingDays <= 0) {
+            parkingFees = 0;
+        } else {
+            printf("\nEnter your total parking fees:");
+            scanf("%d\n", &parkingFees);
+        }
+    } while(carRental < 0 && milesDriven < 0 && parkingDays < 0 && parkingFees < 0);
+
+    printCar(carRental, milesDriven, parkingFees, parkingDays);
+}
